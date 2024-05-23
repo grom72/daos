366d508e561487a409723c0b9ee905ff0f900a27
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-05-23 23:18:30 +0200

    hmm1
    Required-githooks: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- ci/trivy/trivy.sh
+++ ci/trivy/trivy.sh
@@ -3,4 +3,5 @@
 set -eux
 
 trivy fs -c ci/trivy/trivy.yaml --dependency-tree -f table --show-suppressed \
-	--skip-dirs ./src/client/java/hadoop-daos --exit-code 1 .
+
+#	--skip-dirs ./src/client/java/hadoop-daos --exit-code 1 .

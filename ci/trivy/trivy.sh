67419cbef59a1697343eb2823ed459e5926a40f0
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-05-23 22:53:24 +0200

    Remove hadoop files manually
    Required-githooks: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- ci/trivy/trivy.sh
+++ ci/trivy/trivy.sh
@@ -2,5 +2,8 @@
 
 set -eux
 
+# workaround to ignore hadoop files as --skip-dirs option does not work properly on GHA
+rm -rf ./src/client/java/hadoop-daos
+
 trivy fs -c ci/trivy/trivy.yaml --dependency-tree -f table --show-suppressed \
 	--skip-dirs ./src/client/java/hadoop-daos --exit-code 1 .
